cfd262dba1471b77bab558deb02f8bd6b4ebcd4e
furiac3lta <colummarcelo@gmail.com>
2025-11-12 20:50:54 -0300

    Fix CORS for Netlify

--- src/main/java/com/marcedev/attendance/security/config/SecurityConfig.java
+++ src/main/java/com/marcedev/attendance/security/config/SecurityConfig.java
@@ -109,12 +109,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200",
-                "https://attendance-front-swart.vercel.app",  // 🔥 dominio de Vercel
-                "https://gleaming-dodol-e386b2.netlify.app"
-
-        ));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("Authorization", "Cache-Control", "Content-Type"));
         configuration.setExposedHeaders(List.of("Authorization"));
